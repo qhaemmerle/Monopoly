f3245ed686d82f830d2ad6758929b0761ad26d91
tsaulais <123085531+tsaulais@users.noreply.github.com>
2023-01-19 17:21:14 +0100

    Fonction Lancer

--- De.h
+++ De.h
@@ -19,9 +19,12 @@ protected:
 	bool pair;
 
 public:
+	De(int v1=0, int v2=0, bool p = false);
 	~De();
-	int getValeur();
-	int LancerDe();
+	int getValeur1();
+	int getValeur2();
+	int getValeurDe();
+	void LancerDe();
 	bool Double();
 };
 
